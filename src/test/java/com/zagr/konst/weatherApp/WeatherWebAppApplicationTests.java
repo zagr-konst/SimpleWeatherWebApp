c258edc4b2fa9b74e37511b928aa4ee5d1d1e081
Zagr.Konst <zagr.konst@gmail.com>
2022-09-05 21:12:57 +0300

    configured the project, added dependencies

---
+++ src/test/java/com/zagr/konst/weatherApp/WeatherWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zagr.konst.weatherApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
