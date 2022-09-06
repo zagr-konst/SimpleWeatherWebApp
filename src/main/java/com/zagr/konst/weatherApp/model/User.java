e3cbb7283fbc3c4040677cc93ecc7b9678b6a56b
Zagr.Konst <zagr.konst@gmail.com>
2022-09-06 13:10:08 +0300

    created html for user registration, developed User model & Home controller

---
+++ src/main/java/com/zagr/konst/weatherApp/model/User.java
@@ -0,0 +1,36 @@
+package com.zagr.konst.weatherApp.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+
+@Entity
+@Getter @Setter @NoArgsConstructor @EqualsAndHashCode @ToString
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty
+    private String firstName;
+
+    @NotEmpty
+    private String lastName;
+
+    @Email
+    private String email;
+
+    @NotEmpty
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$",
+                message = "password should be stronger")
+    private String password;
+
+    @NotEmpty
+    private String city;
+
+}
